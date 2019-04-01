480fc35a26e2ede1991761d9d96ad26ffe09c2e9
Priit Laupa <priilau@users.noreply.github.com>
2019-04-01 13:35:38 +0300

    Täiendused erinevatele failidele

--- main.js
+++ main.js
@@ -1,45 +1,48 @@
 /*jshint esversion:6*/
 
-let vehicleAdd = document.querySelector("#vehicle");
-let vehicleValue = document.querySelector("#vehicle-name");
-let jarjehoidja = document.querySelector("#jarjehoidja");
-let vehicleTabs = document.querySelector("#vehicle-tabs");
+let categoryAdd = document.querySelector("#vehicle");
+let categoryValue = document.querySelector("#vehicle-type");
+let nimistu = document.querySelector("#nimistu");
+let categoryTabs = document.querySelector("#vehicle-tabs");
 
+let categorys = JSON.parse(localStorage.getItem("categorys"));
 let vehicles = JSON.parse(localStorage.getItem("vehicles"));
 
+if(categorys === null || categorys === undefined) { categorys = []; }
 if(vehicles === null || vehicles === undefined) { vehicles = []; }
 
+console.log(categorys);
 console.log(vehicles);
 
-if(vehicleAdd !== null && vehicleAdd !== undefined) {
-	vehicleAdd.addEventListener("click", function() {
-		let vehicleId = vehicleValue.value.replace(/[^a-z0-9]/gi,'');
+if(categoryAdd !== null && categoryAdd !== undefined) {
+	categoryAdd.addEventListener("click", function() {
+		let categoryId = categoryValue.value.replace(/[^a-z0-9]/gi,'');
 		// kontroll kas juba on olemas.
-		Createvehicle(vehicleId, vehicleValue.value);
-		AddTovehicles(vehicleId, vehicleValue.value);
+		CreateCategory(categoryId, categoryValue.value);
+		AddToCategorys(categoryId, categoryValue.value);
 	});
 }
 
-function Createvehicle(vehicleId, vehicleName) {
-	let existingBlock = document.querySelector("#vehicle-"+vehicleId);
+function CreateCategory(categoryId, categoryName) {
+	let existingBlock = document.querySelector("#category-"+categoryId);
 	if(existingBlock !== null && existingBlock !== undefined) {
-		alert("Selline vehicle on juba olemas!");
+		alert("Selline category on juba olemas!");
 		return;
 	}
 
-	Hidevehicles();
+	HideCategorys();
 
 	// põhi kontainerid jms
-	let vehicleBlock = document.createElement("div");
-	vehicleBlock.classList.add("vehicle");
-	vehicleBlock.classList.add("active");
-	vehicleBlock.id = "vehicle-"+vehicleId;
-	let vehicleLabel = document.createElement("span");
-	vehicleLabel.innerText = vehicleName;
+	let categoryBlock = document.createElement("div");
+	categoryBlock.classList.add("category");
+	categoryBlock.classList.add("active");
+	categoryBlock.id = "category-"+categoryId;
+	let categoryLabel = document.createElement("span");
+	categoryLabel.innerText = categoryName;
 	let vehiclesContainer = document.createElement("div");
 	vehiclesContainer.class = "vehicles";
 
-	let vehicleTable = document.createElement("table");
+	let vehiclesTable = document.createElement("table");
 	let vehicleThead = document.createElement("thead");
 	let vehicleTr = document.createElement("tr");
 	let vehicleTh = document.createElement("th");
@@ -60,7 +63,7 @@ function Createvehicle(vehicleId, vehicleName) {
 	vehiclesContainer.appendChild(vehiclesTable);
 
 	// Raamatu lisamise inputid.
-	let vehiclesName = document.createElement("input");
+	let vehicleName = document.createElement("input");
 	vehicleName.type = "text";
 	vehicleName.className = "vehicle-title";
 	vehicleName.placeholder = "Sõiduki nimi";
@@ -71,56 +74,56 @@ function Createvehicle(vehicleId, vehicleName) {
 	let vehicleDesc = document.createElement("input");
 	vehicleDesc.className = "vehicle-desc";
 	vehicleDesc.placeholder = "Parkimise lõpu kuupäev";
-	vehicleDesc.type = "text";
+	vehicleDesc.type = "date";
 	let vehicleAdd = document.createElement("input");
 	vehicleAdd.type = "button";
-	vehicleAdd.value = "Lisa sõiduk";
+	vehicleAdd.value = "Lisa raamat";
 	let br = document.createElement("br");
-	vehicleLabel.classList.add("active");
+	categoryLabel.classList.add("active");
 
 	// Raamatu lisamine
 	vehicleAdd.addEventListener("click", function() {
-		Createvehicle(vehicleName.value, vehicleLk.value, vehicleDesc.value, vehicleTbody, vehicles.length);
-		AddTovehicles(vehicleId, vehicleName.value, vehicleLk.value, vehicleDesc.value);
+		CreateVehicle(vehicleName.value, vehicleLk.value, vehicleDesc.value, vehicleTbody, vehicles.length);
+		AddToVehicles(categoryId, vehicleName.value, vehicleLk.value, vehicleDesc.value);
 
 		vehicleName.value = "";
 		vehicleLk.value = "";
 		vehicleDesc.value = "";
 	});
 
-	// vehiclei ja selle tabi aktiveerimine
-	vehicleLabel.addEventListener("click", function() {
-		Hidevehicles();
-		vehicleBlock.classList.add("active");
-		vehicleLabel.classList.add("active");
+	// categoryi ja selle tabi aktiveerimine
+	categoryLabel.addEventListener("click", function() {
+		HideCategorys();
+		categoryBlock.classList.add("active");
+		categoryLabel.classList.add("active");
 	});
 
 	let inputContainer = document.createElement("div");
 	inputContainer.className = "vehicle-inputs-container";
-	vehicleTabs.appendChild(vehicleLabel);
+	categoryTabs.appendChild(categoryLabel);
 	inputContainer.appendChild(vehicleName);
 	inputContainer.appendChild(vehicleLk);
 	inputContainer.appendChild(vehicleDesc);
 	inputContainer.appendChild(vehicleAdd);
-	vehicleBlock.appendChild(inputContainer);
+	categoryBlock.appendChild(inputContainer);
 
-	vehicleBlock.appendChild(vehiclesContainer);
-	jarjehoidja.appendChild(vehicleBlock);
+	categoryBlock.appendChild(vehiclesContainer);
+	nimistu.appendChild(categoryBlock);
 }
 
-function AddTovehicles(vehicleId, vehicleTitle, vehiclePage, vehicleDescription) {
-	let vehicle = {"vehicle":vehicleId, "name":vehicleTitle, "lk":vehiclePage, "description":vehicleDescription};
+function AddToVehicles(categoryId, vehicleTitle, vehiclePage, vehicleDescription) {
+	let vehicle = {"category":categoryId, "name":vehicleTitle, "lk":vehiclePage, "description":vehicleDescription};
 	vehicles.push(vehicle);
 	localStorage.setItem("vehicles", JSON.stringify(vehicles));
 }
 
-function AddTovehicles(vehicleId, vehicleName) {
-	let vehicle = {"id":vehicleId, "name":vehicleName};
-	vehicles.push(vehicle);
-	localStorage.setItem("vehicles", JSON.stringify(vehicles));
+function AddToCategorys(categoryId, categoryName) {
+	let category = {"id":categoryId, "name":categoryName};
+	categorys.push(category);
+	localStorage.setItem("categorys", JSON.stringify(categorys));
 }
 
-function Createvehicle(vehicleTitle, vehiclePage, vehicleDescription, tbody, vehicleIndex) {
+function CreateVehicle(vehicleTitle, vehiclePage, vehicleDescription, tbody, vehicleIndex) {
 	let bNameContainer = document.createElement("td");
 	let bName = document.createElement("input");
 	bName.value = vehicleTitle;
@@ -157,7 +160,7 @@ function Createvehicle(vehicleTitle, vehiclePage, vehicleDescription, tbody, veh
 	bDelete.addEventListener("click", function() {
 		vehicles.splice(vehicleIndex, 1);
 		localStorage.setItem("vehicles", JSON.stringify(vehicles));
-		Rendervehicles();
+		RenderVehicles();
 	});
 	let bRow = document.createElement("tr");
 	bRow.className = "vehicle";
@@ -168,36 +171,36 @@ function Createvehicle(vehicleTitle, vehiclePage, vehicleDescription, tbody, veh
 	tbody.appendChild(bRow);
 }
 
-function Rendervehicles() {
-	for(let i = 0, vehicle; vehicle = vehicles[i]; i++) {
-		Createvehicle(vehicle.id, vehicle.name);
+function RenderCategorys() {
+	for(let i = 0, category; category = categorys[i]; i++) {
+		CreateCategory(category.id, category.name);
 	}
 }
 
-function DeleteAllvehicleDOMs() {
+function DeleteAllVehicleDOMs() {
 	let buuks = document.querySelectorAll(".vehicle");
 	for(let i = 0, b; b = buuks[i]; i++) {
 		b.parentElement.removeChild(b);
 	}
 }
 
-function Rendervehicles() {
-	DeleteAllvehicleDOMs();
+function RenderVehicles() {
+	DeleteAllVehicleDOMs();
 	for(let i = 0, vehicle; vehicle = vehicles[i]; i++) {
-		let tbodyElement = document.querySelector("#vehicle-"+vehicle.vehicle+" table tbody");
-		Createvehicle(vehicle.name, vehicle.lk, vehicle.description, tbodyElement, i);
+		let tbodyElement = document.querySelector("#category-"+vehicle.category+" table tbody");
+		CreateVehicle(vehicle.name, vehicle.lk, vehicle.description, tbodyElement, i);
 	}
 }
 
-function Hidevehicles() {
-	let vehicles = document.querySelectorAll(".vehicle");
-	for(let i = 0, vehicle; vehicle = vehicles[i]; i++) {
-		if(vehicle.classList.contains("active")) {
-			vehicle.classList.remove("active");
+function HideCategorys() {
+	let categorys = document.querySelectorAll(".category");
+	for(let i = 0, category; category = categorys[i]; i++) {
+		if(category.classList.contains("active")) {
+			category.classList.remove("active");
 		}
 	}
 
-	let tabs = document.querySelectorAll("#vehicle-tabs span");
+	let tabs = document.querySelectorAll("#category-tabs span");
 	for(let i = 0, tab; tab = tabs[i]; i++) {
 		if(tab.classList.contains("active")) {
 			tab.classList.remove("active");
@@ -206,6 +209,6 @@ function Hidevehicles() {
 }
 
 (function() {
-   Rendervehicles();
-   Rendervehicles();
+   RenderCategorys();
+   RenderVehicles();
 })();

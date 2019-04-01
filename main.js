4d485eb587cde91a44af51ba8e1627bbb59750c5
Priit Laupa <priilau@users.noreply.github.com>
2019-04-01 13:09:05 +0300

    Korrigeeritud main.js

--- main.js
+++ main.js
@@ -1,145 +1,142 @@
 /*jshint esversion:6*/
 
-let zanrAdd = document.querySelector("#zanr");
-let zanrValue = document.querySelector("#zanr-name");
+let vehicleAdd = document.querySelector("#vehicle");
+let vehicleValue = document.querySelector("#vehicle-name");
 let jarjehoidja = document.querySelector("#jarjehoidja");
-let zanrTabs = document.querySelector("#zanr-tabs");
+let vehicleTabs = document.querySelector("#vehicle-tabs");
 
-let zanrs = JSON.parse(localStorage.getItem("zanrs"));
-let books = JSON.parse(localStorage.getItem("books"));
+let vehicles = JSON.parse(localStorage.getItem("vehicles"));
 
-if(zanrs === null || zanrs === undefined) { zanrs = []; }
-if(books === null || books === undefined) { books = []; }
+if(vehicles === null || vehicles === undefined) { vehicles = []; }
 
-console.log(zanrs);
-console.log(books);
+console.log(vehicles);
 
-if(zanrAdd !== null && zanrAdd !== undefined) {
-	zanrAdd.addEventListener("click", function() {
-		let zanrId = zanrValue.value.replace(/[^a-z0-9]/gi,'');
+if(vehicleAdd !== null && vehicleAdd !== undefined) {
+	vehicleAdd.addEventListener("click", function() {
+		let vehicleId = vehicleValue.value.replace(/[^a-z0-9]/gi,'');
 		// kontroll kas juba on olemas.
-		CreateZanr(zanrId, zanrValue.value);
-		AddToZanrs(zanrId, zanrValue.value);
+		Createvehicle(vehicleId, vehicleValue.value);
+		AddTovehicles(vehicleId, vehicleValue.value);
 	});
 }
 
-function CreateZanr(zanrId, zanrName) {
-	let existingBlock = document.querySelector("#zanr-"+zanrId);
+function Createvehicle(vehicleId, vehicleName) {
+	let existingBlock = document.querySelector("#vehicle-"+vehicleId);
 	if(existingBlock !== null && existingBlock !== undefined) {
-		alert("Selline zanr on juba olemas!");
+		alert("Selline vehicle on juba olemas!");
 		return;
 	}
 
-	HideZanrs();
+	Hidevehicles();
 
 	// põhi kontainerid jms
-	let zanrBlock = document.createElement("div");
-	zanrBlock.classList.add("zanr");
-	zanrBlock.classList.add("active");
-	zanrBlock.id = "zanr-"+zanrId;
-	let zanrLabel = document.createElement("span");
-	zanrLabel.innerText = zanrName;
-	let booksContainer = document.createElement("div");
-	booksContainer.class = "books";
+	let vehicleBlock = document.createElement("div");
+	vehicleBlock.classList.add("vehicle");
+	vehicleBlock.classList.add("active");
+	vehicleBlock.id = "vehicle-"+vehicleId;
+	let vehicleLabel = document.createElement("span");
+	vehicleLabel.innerText = vehicleName;
+	let vehiclesContainer = document.createElement("div");
+	vehiclesContainer.class = "vehicles";
 
-	let booksTable = document.createElement("table");
-	let bookThead = document.createElement("thead");
-	let bookTr = document.createElement("tr");
-	let bookTh = document.createElement("th");
-	bookTh.innerText = "Raamatu nimi";
-	bookTr.appendChild(bookTh);
-	bookTh = document.createElement("th");
-	bookTh.innerText = "Lehekülg";
-	bookTr.appendChild(bookTh);
-	bookTh = document.createElement("th");
-	bookTh.innerText = "Lühikirjeldus";
-	bookTr.appendChild(bookTh);
-	bookTh = document.createElement("th"); // btnite jaoks
-	bookTr.appendChild(bookTh);
-	bookThead.appendChild(bookTr);
-	booksTable.appendChild(bookThead);
-	let bookTbody = document.createElement("tbody");
-	booksTable.appendChild(bookTbody);
-	booksContainer.appendChild(booksTable);
+	let vehicleTable = document.createElement("table");
+	let vehicleThead = document.createElement("thead");
+	let vehicleTr = document.createElement("tr");
+	let vehicleTh = document.createElement("th");
+	vehicleTh.innerText = "Sõiduki nimi";
+	vehicleTr.appendChild(vehicleTh);
+	vehicleTh = document.createElement("th");
+	vehicleTh.innerText = "Sõiduki numbrimärk";
+	vehicleTr.appendChild(vehicleTh);
+	vehicleTh = document.createElement("th");
+	vehicleTh.innerText = "Parkimise lõpu kuupäev";
+	vehicleTr.appendChild(vehicleTh);
+	vehicleTh = document.createElement("th"); // btnite jaoks
+	vehicleTr.appendChild(vehicleTh);
+	vehicleThead.appendChild(vehicleTr);
+	vehiclesTable.appendChild(vehicleThead);
+	let vehicleTbody = document.createElement("tbody");
+	vehiclesTable.appendChild(vehicleTbody);
+	vehiclesContainer.appendChild(vehiclesTable);
 
 	// Raamatu lisamise inputid.
-	let bookName = document.createElement("input");
-	bookName.type = "text";
-	bookName.className = "book-title";
-	bookName.placeholder = "Raamatu nimi";
-	let bookLk = document.createElement("input");
-	bookLk.className = "book-lk";
-	bookLk.placeholder = "Leheküljed";
-	bookLk.type = "text";
-	let bookDesc = document.createElement("input");
-	bookDesc.className = "book-desc";
-	bookDesc.placeholder = "Lühikirjeldus";
-	bookDesc.type = "text";
-	let bookAdd = document.createElement("input");
-	bookAdd.type = "button";
-	bookAdd.value = "Lisa raamat";
+	let vehiclesName = document.createElement("input");
+	vehicleName.type = "text";
+	vehicleName.className = "vehicle-title";
+	vehicleName.placeholder = "Sõiduki nimi";
+	let vehicleLk = document.createElement("input");
+	vehicleLk.className = "vehicle-lk";
+	vehicleLk.placeholder = "Sõiduki numbrimärk";
+	vehicleLk.type = "text";
+	let vehicleDesc = document.createElement("input");
+	vehicleDesc.className = "vehicle-desc";
+	vehicleDesc.placeholder = "Parkimise lõpu kuupäev";
+	vehicleDesc.type = "text";
+	let vehicleAdd = document.createElement("input");
+	vehicleAdd.type = "button";
+	vehicleAdd.value = "Lisa sõiduk";
 	let br = document.createElement("br");
-	zanrLabel.classList.add("active");
+	vehicleLabel.classList.add("active");
 
 	// Raamatu lisamine
-	bookAdd.addEventListener("click", function() {
-		CreateBook(bookName.value, bookLk.value, bookDesc.value, bookTbody, books.length);
-		AddToBooks(zanrId, bookName.value, bookLk.value, bookDesc.value);
+	vehicleAdd.addEventListener("click", function() {
+		Createvehicle(vehicleName.value, vehicleLk.value, vehicleDesc.value, vehicleTbody, vehicles.length);
+		AddTovehicles(vehicleId, vehicleName.value, vehicleLk.value, vehicleDesc.value);
 
-		bookName.value = "";
-		bookLk.value = "";
-		bookDesc.value = "";
+		vehicleName.value = "";
+		vehicleLk.value = "";
+		vehicleDesc.value = "";
 	});
 
-	// Zanri ja selle tabi aktiveerimine
-	zanrLabel.addEventListener("click", function() {
-		HideZanrs();
-		zanrBlock.classList.add("active");
-		zanrLabel.classList.add("active");
+	// vehiclei ja selle tabi aktiveerimine
+	vehicleLabel.addEventListener("click", function() {
+		Hidevehicles();
+		vehicleBlock.classList.add("active");
+		vehicleLabel.classList.add("active");
 	});
 
 	let inputContainer = document.createElement("div");
-	inputContainer.className = "book-inputs-container";
-	zanrTabs.appendChild(zanrLabel);
-	inputContainer.appendChild(bookName);
-	inputContainer.appendChild(bookLk);
-	inputContainer.appendChild(bookDesc);
-	inputContainer.appendChild(bookAdd);
-	zanrBlock.appendChild(inputContainer);
+	inputContainer.className = "vehicle-inputs-container";
+	vehicleTabs.appendChild(vehicleLabel);
+	inputContainer.appendChild(vehicleName);
+	inputContainer.appendChild(vehicleLk);
+	inputContainer.appendChild(vehicleDesc);
+	inputContainer.appendChild(vehicleAdd);
+	vehicleBlock.appendChild(inputContainer);
 
-	zanrBlock.appendChild(booksContainer);
-	jarjehoidja.appendChild(zanrBlock);
+	vehicleBlock.appendChild(vehiclesContainer);
+	jarjehoidja.appendChild(vehicleBlock);
 }
 
-function AddToBooks(zanrId, bookTitle, bookPage, bookDescription) {
-	let book = {"zanr":zanrId, "name":bookTitle, "lk":bookPage, "description":bookDescription};
-	books.push(book);
-	localStorage.setItem("books", JSON.stringify(books));
+function AddTovehicles(vehicleId, vehicleTitle, vehiclePage, vehicleDescription) {
+	let vehicle = {"vehicle":vehicleId, "name":vehicleTitle, "lk":vehiclePage, "description":vehicleDescription};
+	vehicles.push(vehicle);
+	localStorage.setItem("vehicles", JSON.stringify(vehicles));
 }
 
-function AddToZanrs(zanrId, zanrName) {
-	let zanr = {"id":zanrId, "name":zanrName};
-	zanrs.push(zanr);
-	localStorage.setItem("zanrs", JSON.stringify(zanrs));
+function AddTovehicles(vehicleId, vehicleName) {
+	let vehicle = {"id":vehicleId, "name":vehicleName};
+	vehicles.push(vehicle);
+	localStorage.setItem("vehicles", JSON.stringify(vehicles));
 }
 
-function CreateBook(bookTitle, bookPage, bookDescription, tbody, bookIndex) {
+function Createvehicle(vehicleTitle, vehiclePage, vehicleDescription, tbody, vehicleIndex) {
 	let bNameContainer = document.createElement("td");
 	let bName = document.createElement("input");
-	bName.value = bookTitle;
-	bName.id = "book-name-"+bookIndex;
+	bName.value = vehicleTitle;
+	bName.id = "vehicle-name-"+vehicleIndex;
 	bName.type = "text";
 	bNameContainer.appendChild(bName);
 	let bLkContainer = document.createElement("td");
 	let bLk = document.createElement("input");
-	bLk.value = bookPage;
-	bLk.id = "book-lk-"+bookIndex;
+	bLk.value = vehiclePage;
+	bLk.id = "vehicle-lk-"+vehicleIndex;
 	bLk.type = "text";
 	bLkContainer.appendChild(bLk);
 	let bDescContainer = document.createElement("td");
 	let bDesc = document.createElement("input");
-	bDesc.value = bookDescription;
-	bDesc.id = "book-desc-"+bookIndex;
+	bDesc.value = vehicleDescription;
+	bDesc.id = "vehicle-desc-"+vehicleIndex;
 	bDesc.type = "text";
 	bDescContainer.appendChild(bDesc);
 	let bBtnContainer = document.createElement("td");
@@ -148,22 +145,22 @@ function CreateBook(bookTitle, bookPage, bookDescription, tbody, bookIndex) {
 	bEdit.type = "button";
 	bEdit.value = "✓";
 	bEdit.addEventListener("click", function() {
-		books[bookIndex].name = bName.value;
-		books[bookIndex].lk = bLk.value;
-		books[bookIndex].description = bDesc.value;
-		localStorage.setItem("books", JSON.stringify(books));
+		vehicles[vehicleIndex].name = bName.value;
+		vehicles[vehicleIndex].lk = bLk.value;
+		vehicles[vehicleIndex].description = bDesc.value;
+		localStorage.setItem("vehicles", JSON.stringify(vehicles));
 	});
 	let bDelete = document.createElement("input");
 	bBtnContainer.appendChild(bDelete);
 	bDelete.type = "button";
 	bDelete.value = "X";
 	bDelete.addEventListener("click", function() {
-		books.splice(bookIndex, 1);
-		localStorage.setItem("books", JSON.stringify(books));
-		RenderBooks();
+		vehicles.splice(vehicleIndex, 1);
+		localStorage.setItem("vehicles", JSON.stringify(vehicles));
+		Rendervehicles();
 	});
 	let bRow = document.createElement("tr");
-	bRow.className = "book";
+	bRow.className = "vehicle";
 	bRow.appendChild(bNameContainer);
 	bRow.appendChild(bLkContainer);
 	bRow.appendChild(bDescContainer);
@@ -171,36 +168,36 @@ function CreateBook(bookTitle, bookPage, bookDescription, tbody, bookIndex) {
 	tbody.appendChild(bRow);
 }
 
-function RenderZanrs() {
-	for(let i = 0, zanr; zanr = zanrs[i]; i++) {
-		CreateZanr(zanr.id, zanr.name);
+function Rendervehicles() {
+	for(let i = 0, vehicle; vehicle = vehicles[i]; i++) {
+		Createvehicle(vehicle.id, vehicle.name);
 	}
 }
 
-function DeleteAllBookDOMs() {
-	let buuks = document.querySelectorAll(".book");
+function DeleteAllvehicleDOMs() {
+	let buuks = document.querySelectorAll(".vehicle");
 	for(let i = 0, b; b = buuks[i]; i++) {
 		b.parentElement.removeChild(b);
 	}
 }
 
-function RenderBooks() {
-	DeleteAllBookDOMs();
-	for(let i = 0, book; book = books[i]; i++) {
-		let tbodyElement = document.querySelector("#zanr-"+book.zanr+" table tbody");
-		CreateBook(book.name, book.lk, book.description, tbodyElement, i);
+function Rendervehicles() {
+	DeleteAllvehicleDOMs();
+	for(let i = 0, vehicle; vehicle = vehicles[i]; i++) {
+		let tbodyElement = document.querySelector("#vehicle-"+vehicle.vehicle+" table tbody");
+		Createvehicle(vehicle.name, vehicle.lk, vehicle.description, tbodyElement, i);
 	}
 }
 
-function HideZanrs() {
-	let zanrs = document.querySelectorAll(".zanr");
-	for(let i = 0, zanr; zanr = zanrs[i]; i++) {
-		if(zanr.classList.contains("active")) {
-			zanr.classList.remove("active");
+function Hidevehicles() {
+	let vehicles = document.querySelectorAll(".vehicle");
+	for(let i = 0, vehicle; vehicle = vehicles[i]; i++) {
+		if(vehicle.classList.contains("active")) {
+			vehicle.classList.remove("active");
 		}
 	}
 
-	let tabs = document.querySelectorAll("#zanr-tabs span");
+	let tabs = document.querySelectorAll("#vehicle-tabs span");
 	for(let i = 0, tab; tab = tabs[i]; i++) {
 		if(tab.classList.contains("active")) {
 			tab.classList.remove("active");
@@ -209,6 +206,6 @@ function HideZanrs() {
 }
 
 (function() {
-   RenderZanrs();
-   RenderBooks();
+   Rendervehicles();
+   Rendervehicles();
 })();

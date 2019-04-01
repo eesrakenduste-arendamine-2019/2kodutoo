14f862dbb01272427c05cea25089a2918db57e0c
Marvin Helstein <17002425+KareVulli@users.noreply.github.com>
2019-04-01 12:57:38 +0300

    Implemented local storage persistance.

--- index.js
+++ index.js
@@ -1,49 +1,94 @@
-/*jshint esversion:6*/
+/* jshint esversion:6 */
 
-class Todo{
-  constructor(title,description,date){
-    this.title=title;
-    this.description=description;
-    this.date=date;
-    this.done=false;
-  }
+class Todo {
+    constructor(title, description, date, done = false) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.done = done;
+    }
 }
 
-let todos=[];
+let todos = [];
 
-$('#addButton').on('click',()=>addEntry());
-$('#saveButton').on('click',()=>saveToFile());
-$('#loadButton').on('click',()=>render());
+$(function() { // Same as $(document).ready();
+    var now = new Date();
+    var day = ("0" + now.getDate()).slice(-2);
+    var month = ("0" + (now.getMonth() + 1)).slice(-2);
+    today = now.getFullYear()+"-"+(month)+"-"+(day) ;
 
-function render(){
-  $('#todos').html("");
-  $.get('database.txt', function(data){
-    let content=JSON.parse(data).content;
+    $('#date').val(today);
 
-    content.forEach(function(todo, todoIndex){
-      console.log(todoIndex);
-      $('#todos').append('<ul><li>'+ todo.title+'</li><li>'+ todo.description+'</li><li>'+todo.date+'</li></ul>');
+    $('#addButton').on('click', () => addEntry());
+    $('#saveButton').on('click', () => {
+        saveToFile();
+        saveToLocalStorage();
     });
-  });
+    $('#loadButton').on('click', () => loadFromFile());
+
+
+    loadFromLocalStorage();
+});
+
+
+
+function render() {
+    $('#todos').html("");
+    todos.forEach(function (todo, todoIndex) {
+        console.log(todoIndex);
+        $('#todos').append('<ul><li>' + todo.title + '</li><li>' + todo.description + '</li><li>' + todo.date + '</li></ul>');
+    });
+    
 }
 
 
-function addEntry(){
-  const titleValue = $('#title').val();
-  const dateValue = $('#date').val();
-  const descriptionValue = $('#description').val();
+function addEntry() {
+    const titleValue = $('#title').val();
+    const dateValue = $('#date').val();
+    const descriptionValue = $('#description').val();
 
-  todos.push(new Todo(titleValue,descriptionValue,dateValue));
+    todos.push(new Todo(titleValue, descriptionValue, dateValue));
 
-  console.log(todos);
+    console.log(todos);
+    render();
 }
 
-function saveToFile(){
-  $.post('server.php', {save:todos}).done(function(){
-    console.log('done');
-  }).fail(function(){
-    console.log('fail');
-  }).always(function(){
-    console.log('always');
-  });
+function saveToFile() {
+    $.post('server.php', { save: todos }).done(function () {
+        console.log('done');
+    }).fail(function () {
+        console.log('fail');
+    }).always(function () {
+        console.log('always');
+    });
+}
+
+function loadFromFile() {
+    $.getJSON('database.txt', function (data) {
+        todos = arrayToTodoItems(data.content);
+        console.log(todos);
+        render();
+    });
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function loadFromLocalStorage() {
+    let items = localStorage.getItem('todos');
+    if (items) {
+        todos = arrayToTodoItems(JSON.parse(items));
+        console.log(todos);
+        render();
+    }
+    
+}
+
+function arrayToTodoItems(items) {
+    let objects = [];
+    items.forEach(todo => {
+        objects.push(new Todo(todo.title, todo.description, title.date, title.done));
+    });
+    return objects;
 }
--- server.php
+++ server.php
@@ -1,14 +1,16 @@
 <?php
-  if(isset($_POST["save"]) && !empty($_POST["save"])){
-    saveToFile($_POST["save"]);
-  }
-  function saveToFile($stringToSave){
-    $object=new StdClass();
-    $object->last_modified=time();
-    $object->content =$stringToSave;
-    $jsonString=json_encode($object);
-    if(file_put_contents("database.txt",$jsonString)){
-      echo "SUCCess";
+    if (isset($_POST["save"]) && !empty($_POST["save"])) {
+        saveToFile($_POST["save"]);
     }
-  }
+
+    function saveToFile($stringToSave)
+    {
+        $object=new StdClass();
+        $object->last_modified=time();
+        $object->content =$stringToSave;
+        $jsonString=json_encode($object);
+        if (file_put_contents("database.txt",$jsonString)) {
+            echo "SUCCess";
+        }
+    }
  ?>

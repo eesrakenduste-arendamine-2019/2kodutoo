291a377eaf0742b328b57f4ed717319ea4937815
Gertin Pakkonen <gertpak@users.noreply.github.com>
2019-04-07 16:09:18 +0300

    Lisatud local storage salvestamine poolik

--- index.js
+++ index.js
@@ -13,19 +13,23 @@ let todos = [];
 $('#addButton').on("click", ()=> addEntry());
 $('#saveButton').on("click", ()=> saveToFile());
 $('#loadButton').on("click", ()=> render());
-
+let content;
 function render(){
     $('#todos').html("");
     $.get("database.txt", function(data){
-        let content = JSON.parse(data).content;
+        content = JSON.parse(data).content;
 
         content.forEach(function(todo, todoIndex){
             //prepend on ette
-            $('#todos').append('<ul><li>'+todoIndex+'</li><li>'+todo.title+'</li><li>'+todo.description+'</li><li>'+todo.date+'</li></ul>');
-        });
+            $('#todos').append('<ul><li>'+todoIndex+'</li><li>'+todo.title+'</li><li>'+todo.description+'</li><li>'+todo.date+'</li><div class="delete-task-button">×</div></ul>');
+            saveInLocalStorage();
+         });
+
     });
 }
-
+function saveInLocalStorage(){
+    window.localStorage.setItem('content', JSON.stringify(content));
+}
 function addEntry(){
     const titleValue = $('#title').val();
     const descriptionValue = $('#description').val();

a11fe16145e81efcfae3630f6de93a1b762b4b3a
Krispol <kristjan.poldmets@gmail.com>
2019-04-12 18:25:29 +0300

    Katki oli

--- main.js
+++ main.js
@@ -240,23 +240,23 @@ function HideCategorys() {
 	}
 }
 
-function GetCategories(){
-	$.get('categories.txt', function(data){
-    let content = JSON.parse(data).content;
-    content.forEach(function(categoryId, categoryName){
-      console.log(categoryId);
-      $('#todos').append('<ul><li>'+ todo.title+'</li><li>'+ todo.description+'</li><li>'+ todo.date+'</li></ul>');
-  });
-});
-
-function GetVehicles(){
-	$.get('vehicles.txt', function(data){
-    let content = JSON.parse(data).content;
-    content.forEach(function(categoryId, vehicleName.value, vehicleNum.value, vehicleDate.value){
-      console.log(todoIndex);
-      $('#todos').append('<ul><li>'+ todo.title+'</li><li>'+ todo.description+'</li><li>'+ todo.date+'</li></ul>');
-  });
-});
+// function GetCategories(){
+// 	$.get('categories.txt', function(data){
+//     let content = JSON.parse(data).content;
+//     content.forEach(function(categoryId, categoryName){
+//       console.log(categoryId);
+//       $('#todos').append('<ul><li>'+ todo.title+'</li><li>'+ todo.description+'</li><li>'+ todo.date+'</li></ul>');
+//   });
+// });
+//
+// function GetVehicles(){
+// 	$.get('vehicles.txt', function(data){
+//     let content = JSON.parse(data).content;
+//     content.forEach(function(categoryId, vehicleName.value, vehicleNum.value, vehicleDate.value){
+//       console.log(todoIndex);
+//       $('#todos').append('<ul><li>'+ todo.title+'</li><li>'+ todo.description+'</li><li>'+ todo.date+'</li></ul>');
+//   });
+// });
 
 (function() {
    RenderCategorys();

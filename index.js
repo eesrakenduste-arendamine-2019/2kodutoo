131feb8210e9952a083d7f6640adf61e7d441adf
raimop <43129557+raimop@users.noreply.github.com>
2019-04-13 00:42:13 +0300

    Uuendatud osa

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 /*jshint esversion:6*/
 
+let getTodo = localStorage.getItem('todo');
+
+$(document).ready(function(){
+	if (getTodo != null){
+		showResults();
+	}
+});
+
 class Todo{
   constructor(task, date){
     this.task = task;
@@ -32,14 +40,82 @@ function addEntry(){
 
   todos.push(new Todo(taskValue, dateValue));
 
-  console.log(todos);
+  //console.log(todos);
 }
   function saveToFile(){
     $.post('server.php', {save: todos}).done(function(){
-      alert('done');
+      //alert('done');
     }).fail(function(){
       alert('fail');
     }).always(function(){
-      console.log('works');
+      //console.log('works');
     });
+	
+	saveInLocalStorage();
   }
+
+function saveInLocalStorage(){
+	
+	let massif = [];
+	
+    let todoObject = { 'task': $('#task').val(), 'date': $('#date').val(), 'done': false };
+
+    let previousTodos = JSON.parse(localStorage.getItem('todo'));
+
+    if (previousTodos != null){
+		$.each(previousTodos, function(index, value){
+			massif.push(value);
+		});
+    }
+	
+	/*$.each(todos, function(index, value){
+		massif.push(value);
+	});*/
+	
+	massif.push(todoObject);
+
+    localStorage.setItem('todo', JSON.stringify(massif));
+	
+    showResults();
+}
+  
+function showResults(){
+	$('#todos').html("");
+    let previousTodos = JSON.parse(localStorage.getItem('todo'));
+	let newMassif = [];
+
+    $.each( previousTodos, function( key, value ) {
+		if (value.date == ""){
+			value.date = "0000-00-00";
+		}
+		
+		newMassif.push(value);
+    });
+	
+	newMassif.sort(function (a, b){
+		if (parseInt(a.date.slice(0, 4)) != parseInt(b.date.slice(0, 4))){
+			return parseInt(a.date.slice(0, 4)) - parseInt(b.date.slice(0, 4));
+		} else if (parseInt(a.date.slice(5, 7)) != parseInt(b.date.slice(5, 7))){
+			return parseInt(a.date.slice(5, 7)) - parseInt(b.date.slice(5, 7));
+		} else {
+			return parseInt(a.date.slice(8, 10)) - parseInt(b.date.slice(8, 10));
+		}
+	});
+	
+	/*newMassif.sort(function (a, b){
+		return parseInt(a.date.slice(6, 7)) - parseInt(b.date.slice(6, 7));
+	});
+	
+	newMassif.sort(function (a, b){
+		return parseInt(a.date.slice(8, 9)) - parseInt(b.date.slice(8, 9));
+	});*/
+	
+	$.each( newMassif, function (key, value) {
+		$('#todos').append("<li>" + value.task + " " + value.date + " " + value.done + "</li>");
+	});
+	
+	/*$.each( newMassif, function( key, value) {
+		console.log(value.task + " " + value.date + " " + value.done + "\n");
+	});*/
+
+}

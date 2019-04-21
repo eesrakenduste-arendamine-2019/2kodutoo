a79ecdf9e52c20573c4400a847d687f16c7adf48
Krispol <kristjan.poldmets@gmail.com>
2019-04-21 21:57:18 +0300

    Pullimiseks

--- main.js
+++ main.js
@@ -349,25 +349,35 @@ function CompareVehicles(){
 	});
 }
 
-// function saveCategory(categoryLabel){
-// 	console.log("Jõudis saveCategory funktsiooni");
-// 	var data = new FormData();
-// 		data.append("name", categoryLabel.value);
-//
-// 		var req = new XMLHttpRequest();
-// 		req.open('POST', "server.php");
-// 		req.onload = function() {
-// 			if (req.status == 200) {
-// 				console.log(req.response);
-// 			}
-// 		};
-//
-// 		req.onerror = function() {
-// 			console.log("Error Network Error");
-// 		};
-//
-// 		req.send(data);
-// }
+
+
+function saveCategory(categoryLabel){
+	console.log("Jõudis saveCategory funktsiooni");
+	var data = new FormData();
+		data.append("name", categoryLabel.value);
+
+		var req = new XMLHttpRequest();
+		req.open('POST', "server.php");
+
+		xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    	}
+  	};
+
+		req.onload = function() {
+			if (req.status == 200) {
+				console.log(req.response);
+			}
+		};
+
+		req.onerror = function() {
+			console.log("Error Network Error");
+		};
+
+		req.send(data);
+}
 //
 // function saveToFile(){
 //     console.log("Jõudis saveToFile funktsiooni");
--- server.php
+++ server.php
@@ -1,16 +1,20 @@
 <?php
+  var_dump($_POST);die;
   echo "Jõudis server.php'sse";
-  if(isset($_POST["save"]) && !empty($_POST["save"])){
-    saveCategory($_POST["save"]);
-  }
-  function saveCategory($stringToSave){
-    $object = new StdClass();
-    $object->last_modified = time();
-    $object->content = $stringToSave;
-    $jsonString = json_encode($object);
-    if(file_put_contents("categories.txt", $jsonString)){
-      echo "success";
-    }
-  }
+
+  // if(isset($_POST[""]))
+  //
+  // if(isset($_POST["save"]) && !empty($_POST["save"])){
+  //   saveToFile($_POST["save"]);
+  // }
+  // function saveToFile($stringToSave){
+  //   $object = new StdClass();
+  //   $object->last_modified = time();
+  //   $object->content = $stringToSave;
+  //   $jsonString = json_encode($object);
+  //   if(file_put_contents("categories.txt", $jsonString)){
+  //     echo "success";
+  //   }
+  // }
 
 ?>

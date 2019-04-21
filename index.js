0e798fd7d85766116ba640920b79a0879978f8b8
Karl-Daniel Karu <43164719+kdkaru@users.noreply.github.com>
2019-04-21 20:47:07 +0300

    nimede jargi sorteerimine

--- index.js
+++ index.js
@@ -118,4 +118,26 @@ function showTodos(){
     $('#todos').listview('refresh');
   });
 }
+
+function sortUnorderedList(ul, sortDescending) {
+  if(typeof ul == "string")
+    ul = document.getElementById(ul);
+    var lis = ul.getElementsByTagName("LI");
+    var vals = [];
+    for(var i = 0, l = lis.length; i < l; i++)
+      vals.push(lis[i].innerHTML);
+      vals.sort();
+  if(sortDescending)
+    vals.reverse();
+    for(var i = 0, l = lis.length; i < l; i++)
+      lis[i].innerHTML = vals[i];
+  }
+  window.onload = function() {
+    var desc = false;
+    document.getElementById("sortByName").onclick = function() {
+      sortUnorderedList("todos", desc);
+      desc = !desc;
+      return false;
+  };
+  };
 });
